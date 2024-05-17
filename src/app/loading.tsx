6d2f43494540b00ff79dc33bfea89d28c751e151
Al Amin <alamin931761@gmail.com>
2024-05-17 15:05:15 +0600

    loading page done

---
+++ src/app/loading.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+const loading = () => {
+  return (
+    <div className="h-[calc(100vh-64px)] flex justify-center items-center">
+      <Image
+        src="https://i.ibb.co/qdzSdkQ/loading.gif"
+        width={100}
+        height={100}
+        alt="loading"
+      />
+    </div>
+  );
+};
+
+export default loading;
